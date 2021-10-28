c83a3fbacabb181037ad35733950dd1631d3cc56
ScriptKiddies010 <scriptkiddies010@gmail.com>
2021-10-28 18:51:00 -0500

    Actualización

--- src/comision/Comision.java
+++ src/comision/Comision.java
@@ -42,7 +42,7 @@ import java.util.Arrays;
   //Declaramos e inicializamos los contadores
         int a=0,b=0,c=0,d=0,ee=0,f=0,g=0,h=0,ii=0;
   
-        int vta = 0,i;
+        int vta = 0,i, vendedor=1, vendedoralert=0;
   //Declaramos los objetos
     JButton btsig, btcalcular;
     JLabel lbventas;
@@ -57,7 +57,7 @@ import java.util.Arrays;
             
             btsig= new JButton("Registrar");
             btcalcular= new JButton("Calcular");
-            lbventas= new JLabel("Ingresa las ventas totales de esta semana: ");
+            lbventas= new JLabel("Ingresa las ventas totales de esta semana del vendedor numero 1: ");
             tfventas= new JTextField(5);
          
             
@@ -66,14 +66,13 @@ import java.util.Arrays;
             contenedor.add(btsig);
             contenedor.add(btcalcular);
             
-            
             btsig.addActionListener(this);
             setVisible (true);
-            setSize(200,300);
-            
+                    
             btcalcular.addActionListener(this);
             setVisible (true);
-            setSize(200,300);
+            setSize(700,100);
+            
     }
     
     public void actionPerformed(ActionEvent e){
@@ -81,18 +80,21 @@ import java.util.Arrays;
         
       
      
-        //Si presionamos el btsig se calcula la comision y se le agregan $200
+        //Si presionamos el boton registrar se calcula la comision y se le agregan $200
         if(e.getSource()==btsig){
+        vendedor=vendedor+1;
+        vendedoralert=vendedoralert+1;
+        
         vta=Integer.parseInt(tfventas.getText());
+        lbventas.setText("Ingresa las ventas totales de esta semana del vendedor numero "+vendedor+": ");
+        
         
         vta=(int)(vta*0.09F+200);
-        System.out.println("Total de la venta: "+vta);
+        //System.out.println("Total de la venta: "+vta);
         //Borramos el campo de texto para permitir ingresar un nuevo numero
-        tfventas.setText(null);
-     
        
-        } //cierrra if
-   
+       
+        
         //
           //este if nos ayuda a comparar entre 2 cantidades
          if (vta >= 200 && vta <= 299){
@@ -100,22 +102,22 @@ import java.util.Arrays;
             a=a+1;
             //asignamos valores al arreglo
             rango[0]= "a) 200 - 299 : "+a+"\n";
-             System.out.println(rango[0]);
+             //System.out.println(rango[0]);
         }
          else if(vta >= 300 && vta <= 399) {
          b=b+1;
             rango[1]= "b) 300 - 399 : "+b+"\n";
-             System.out.println(rango[1]);
+             //System.out.println(rango[1]);
         }
         else if(vta >= 400 && vta <= 499) {
           c=c+1;
             rango[2]= "c) 400 - 499 : "+c+"\n";
-             System.out.println(rango[2]);
+             //System.out.println(rango[2]);
         }
          else if(vta >= 500 && vta <= 599) {
           d=d+1;
             rango[3]= "d) 500 - 599 : "+d+"\n";
-             System.out.println(rango[3]);
+             //System.out.println(rango[3]);
         }
         else if(vta >= 600 && vta <= 699) {
             ee=ee+1;
@@ -125,28 +127,41 @@ import java.util.Arrays;
         else if(vta >= 700 && vta <= 799) {
           f=f+1;
             rango[5]= "f) 700 - 799 : "+f+"\n";
-             System.out.println(rango[5]);
+             //System.out.println(rango[5]);
         }
         else if(vta >= 800 && vta <= 899) {
          g=g+1;
             rango[6]= "g) 800 - 899 : "+g+"\n";
-             System.out.println(rango[6]);
+             //System.out.println(rango[6]);
         }
          else if(vta >= 900 && vta <= 999) {
         h=h+1;
             rango[7]= "h) 900 - 999 : "+h+"\n";
-             System.out.println(rango[7]);
+             //System.out.println(rango[7]);
         }
         else if(vta >= 1000) {
              ii=ii+1;
             rango[8]= "i) 1000 - en adelante : "+ii+"\n";
-             System.out.println(rango[8]);
+             //System.out.println(rango[8]);
         }
-        
+         
+          tfventas.setText(null);
+        JOptionPane.showMessageDialog(null, "Total de la venta: "+vta+"\n"+"Se registro el vendedor: "+vendedoralert);
+
+         } //cierrra if
+   
+         
+        //Si se presiona el boton calcular
            if(e.getSource()==btcalcular){
                     System.out.println("Rango          Cantidad de vendedores");
+                    
+           
              for (int i = 0; i < 9; i++) {
+                 
+                 //Si no existen registros de ese rango no imprime y sale del for
+                  if(rango[i]!=null){
 			System.out.print(" "+rango[i]+"\n");
+                  }
 			
 		}
       
