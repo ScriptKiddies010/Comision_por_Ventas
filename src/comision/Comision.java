af0cd4d99473f7910841e0c7fc4d41cab8dde87b
ScriptKiddies010 <scriptkiddies010@gmail.com>
2021-10-28 13:35:35 -0500

    Se agrega el Proyecto

---
+++ src/comision/Comision.java
@@ -0,0 +1,154 @@
+/*
+Comision por ventas()
+Utilice un arreglo de una dimension para resolver el siguiente problema: 
+
+Una compañia paga a sus vendedores por comision. 
+
+Los vendedores reciben 200$ por semana mas el 9% de sus ventas totales de esta semana. 
+
+Por ejemplo un vendedor que acumule,5000$ en ventas en una semana recibira 200$ mas el 9% de 5000$, 
+a un total de 650$. 
+
+Escriba una app (Utilizar un arreglo de contadores) que determine cuantos venderodres recibieron el 
+salario en cada uno de los siguientes rangos.
+
+
+(suponga que el salario de c/ vendedor se trunca a una cantidad entera):
+a) 200 - 299
+b) 300 - 399
+c) 400 - 499
+d) 500 - 599
+e) 600 - 699
+f) 700 - 799
+g) 800 - 899
+h) 900 - 999
+i) 1000 - en adelante
+
+Muestre los datos que se piden mediante, la clase System.out.Print
+ */
+package comision;
+
+import javax.swing.*;
+import java.awt.Container;
+import java.awt.FlowLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.util.Arrays;
+
+ class Comision extends JFrame implements ActionListener{
+     
+  //declaramos el arreglo rangos_venta
+        String [] rango = new String[9];
+        int a=0,b=0,c=0,d=0,ee=0,f=0,g=0,h=0,ii=0;
+        int vta = 0,i;
+       
+    JButton btsig, btcalcular;
+    JLabel lbventas;
+    JTextField tfventas;
+    
+    public Comision(){
+        super("Comision");
+        Container contenedor=getContentPane();
+            contenedor.setLayout(new FlowLayout());
+            
+            btsig= new JButton("Registrar");
+            btcalcular= new JButton("Calcular");
+            lbventas= new JLabel("Ingresa las ventas totales de esta semana: ");
+            tfventas= new JTextField(5);
+         
+            
+            contenedor.add(lbventas);
+            contenedor.add(tfventas);
+            contenedor.add(btsig);
+            contenedor.add(btcalcular);
+            
+            
+            btsig.addActionListener(this);
+            setVisible (true);
+            setSize(200,300);
+            
+            btcalcular.addActionListener(this);
+            setVisible (true);
+            setSize(200,300);
+    }
+    
+    public void actionPerformed(ActionEvent e){
+         //se inicializa el arreglo
+        
+      
+     
+        
+        if(e.getSource()==btsig){
+        vta=Integer.parseInt(tfventas.getText());
+        vta=(int)(vta*0.09F+200);
+        System.out.println("Total de la venta: "+vta);
+        //Borramos el campo de texto para permitir ingresar un nuevo numero
+        tfventas.setText(null);
+     
+       
+        } //cierrra if
+   
+          //este if nos ayuda a comparar entre 2 cantidades
+         if (vta >= 200 && vta <= 299){
+            a=a+1;
+            rango[0]= "a) 200 - 299 : "+a+"\n";
+             System.out.println(rango[0]);
+        }
+         else if(vta >= 300 && vta <= 399) {
+         b=b+1;
+            rango[1]= "b) 300 - 399 : "+b+"\n";
+             System.out.println(rango[1]);
+        }
+        else if(vta >= 400 && vta <= 499) {
+          c=c+1;
+            rango[2]= "c) 400 - 499 : "+c+"\n";
+             System.out.println(rango[2]);
+        }
+         else if(vta >= 500 && vta <= 599) {
+          d=d+1;
+            rango[3]= "d) 500 - 599 : "+d+"\n";
+             System.out.println(rango[3]);
+        }
+        else if(vta >= 600 && vta <= 699) {
+            ee=ee+1;
+            rango[4]= "e) 600 - 699 : "+ee+"\n";
+             System.out.println(rango[4]);
+        }
+        else if(vta >= 700 && vta <= 799) {
+          f=f+1;
+            rango[5]= "f) 700 - 799 : "+f+"\n";
+             System.out.println(rango[5]);
+        }
+        else if(vta >= 800 && vta <= 899) {
+         g=g+1;
+            rango[6]= "g) 800 - 899 : "+g+"\n";
+             System.out.println(rango[6]);
+        }
+         else if(vta >= 900 && vta <= 999) {
+        h=h+1;
+            rango[7]= "h) 900 - 999 : "+h+"\n";
+             System.out.println(rango[7]);
+        }
+        else if(vta >= 1000) {
+             ii=ii+1;
+            rango[8]= "i) 1000 - en adelante : "+ii+"\n";
+             System.out.println(rango[8]);
+        }
+        
+           if(e.getSource()==btcalcular){
+                    System.out.println("Rango          Cantidad de vendedores");
+             for (int i = 0; i < 9; i++) {
+			System.out.print(" "+rango[i]+"\n");
+			
+		}
+      
+       
+        }
+        
+    } //cierra actionPreformed
+    
+    public static void main(String[] args) {
+        Comision g=new Comision();
+    }
+    
+}
